e7db6abe5d4a6ad6bfc35a5761ab55e9dc919e17
jmyanish <jmy5844@nyu.edu>
2022-03-08 10:29:36 -0500

    Updated Code
    Updated with additional code + fixes per the lab-rest-tdd document and testing. have not completed testing yet, waiting on model

--- service/routes.py
+++ service/routes.py
@@ -7,6 +7,7 @@ Describe what your service does here
 import os
 import sys
 import logging
+from werkzeug.exceptions import NotFound
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from . import status  # HTTP Status Codes
 
@@ -43,19 +44,36 @@ def init_db():
 # UPDATE AN EXISTING CUSTOMER
 ######################################################################
 @app.route("/customers/<int:customer_id>", methods=["PUT"])
-defupdate_customer(customer_id):
-"""Update a Customer
+def update_customer(customer_id):
+    """Update a Customer
 
     This endpoint will update a Customer based the body that is posted
     """
-app.logger.info("Request to update customer with id: %s", customer_id)
-check_content_type("application/json")
-customer=customer.find(customer_id)
-ifnotcustomer:
-raiseNotFound("Customer with id '{}' was not found.".format(customer_id))
-customer.deserialize(request.get_json())
-customer.id=customer_id
-customer.update()
+    app.logger.info("Request to update customer with id: %s", customer_id)
+    check_content_type("application/json")
+    customer=customer.find(customer_id)
+    if not customer:
+        raise NotFound("Customer with id '{}' was not found.".format(customer_id))
+    customer.deserialize(request.get_json())
+    customer.id=customer_id
+    customer.update()
 
-app.logger.info("Customerwith ID [%s] updated.", customer.id)
-returnmake_response(jsonify(customer.serialize()), status.HTTP_200_OK)
+    app.logger.info("Customerwith ID [%s] updated.", customer.id)
+    return make_response(jsonify(customer.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,6 +13,27 @@ from service import status  # HTTP Status Codes
 from service.models import db
 from service.routes import app, init_db
 
+import os
+import logging
+import unittest
+
+# from unittest.mock import MagicMock, patch
+from urllib.parse import quote_plus
+from service import app, status
+from service.models import db, init_db
+from .factories import CustomerFactory
+
+# Disable all but critical errors during normal test run
+# uncomment for debugging failing tests
+logging.disable(logging.CRITICAL)
+
+# DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
+BASE_URL = "/customers"
+CONTENT_TYPE_JSON = "application/json"
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -45,3 +66,26 @@ class TestYourResourceServer(TestCase):
         """ Test index call """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+  # Test update the customer
+    def test_update_customer(self):
+        """Update an existing customer"""
+        # create a customer to update
+        test_customer = CustomerFactory()
+        resp = self.app.post(
+            BASE_URL, json=test_customer.serialize(), content_type=CONTENT_TYPE_JSON
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the customer
+        new_customer = resp.get_json()
+        logging.debug(new_customer)
+        new_customer["category"] = "unknown"
+        resp = self.app.put(
+            "/customer/{}".format(new_customer["id"]),
+            json=new_customer,
+            content_type=CONTENT_TYPE_JSON,
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_customer = resp.get_json()
+        self.assertEqual(updated_customer["category"], "unknown")
