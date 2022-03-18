1e118d63c58597c71b5d53aa8f2a1a88cb8ba9ac
norabuccinonyu <93152030+norabuccinonyu@users.noreply.github.com>
2022-03-18 16:39:59 -0400

    Refactor code and fix tests with errors
    Refactored code in routes.py and test_routes.py to remove duplicative import statements and comments. Updated test_routes.py to fix error from test_update_pet test case.

--- service/routes.py
+++ service/routes.py
@@ -1,81 +1,69 @@
 """
-My Service
+Customer Service
 
-Describe what your service does here
+Paths:
+------
+GET /customers - Returns a list all of the Customers
+GET /customers/{id} - Returns the Customer with a given id number
+POST /customers - creates a new Customer record in the database
+PUT /customers/{id} - updates a Customer record in the database
+DELETE /customers/{id} - deletes a Customer record in the database
 """
 
-import os
-import sys
-import logging
-from werkzeug.exceptions import NotFound
 from flask import Flask, jsonify, request, url_for, make_response, abort
-from . import status  # HTTP Status Codes
-
-# For this example we'll use SQLAlchemy, a popular ORM that supports a
-# variety of backends including SQLite, MySQL, and PostgreSQL
-from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import NotFound
 from service.models import Customer, DataValidationError
-
-# Import Flask application
-from . import app
+from . import status  # HTTP Status Codes
+from . import app # Import Flask application
 
 ######################################################################
 # GET INDEX
 ######################################################################
-
-
 @app.route("/")
 def index():
-    """Returns information about the service"""
-    app.logger.info("Request for Base URL")
+    """Root URL response"""
+    app.logger.info("Request for Root URL")
     return (
         jsonify(
-            message="Customer Service",
-            version="1.0.0",
+            name="Customer Service",
+            version="1.0",
             url=url_for("list_customers", _external=True),
         ),
         status.HTTP_200_OK,
     )
 
+
 ######################################################################
-# RETRIEVE A CUSTOMER
+# LIST ALL CUSTOMERS
 ######################################################################
-
-
-@app.route("/customers/<int:customer_id>", methods=["GET"])
-def get_customers(customer_id):
-    """
- #   Retrieve a single Customer
- #   This endpoint will return a Customer based on it's id
- #   """
-    app.logger.info("Request for customer with id: %s", customer_id)
-    customer = Customer.find(customer_id)
-    if not customer:
-        raise NotFound(
-            "customer with id '{}' was not found.".format(customer_id))
-
-    app.logger.info("Returning customer: %s", customer.first_name)
-    return make_response(jsonify(customer.serialize()), status.HTTP_200_OK)
-
-############################################################
-#                 R E S T   A P I
-############################################################
-
-
-# -----------------------------------------------------------
-# List customers
-# -----------------------------------------------------------
-@app.route("/customers", methods=[""])
+@app.route("/customers", methods=["GET"])
 def list_customers():
     return ""
 
-# -----------------------------------------------------------
-# Create customer
-# -----------------------------------------------------------
 
+######################################################################
+# RETRIEVE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["GET"])
+def get_customers(customer_id):
+    """
+    Retrieve a single Customer
 
+    This endpoint will return a Customer based on it's id
+    """
+    app.logger.info("Request for customer with id: %s", customer_id)
+    customer = Customer.find(customer_id)
+    if not customer:
+        raise NotFound("customer with id '{}' was not found.".format(customer_id))
+
+    app.logger.info("Returning customer: %s %s", customer.first_name, customer.last_name)
+    return make_response(jsonify(customer.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# CREATE A NEW CUSTOMER
+######################################################################
 @app.route("/customers", methods=["POST"])
-def create_customer():
+def create_customers():
     """
     Creates a Customer Account
     This endpoint will create a Customer Account based on the data in the body that is posted
@@ -86,39 +74,18 @@ def create_customer():
     customer.deserialize(request.get_json())
     customer.create()
     message = customer.serialize()
-    location_url = url_for(
-        "get_customers", customer_id=customer.id, _external=True)
+    location_url = url_for("get_customers", customer_id=customer.id, _external=True)
+    
     app.logger.info("Customer with ID [%s] created", customer.id)
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-# -----------------------------------------------------------
-# Delete customer
-# -----------------------------------------------------------
-
-
-@app.route("/", methods=[""])
-def delete_customer(name):
-    return ""
-
-
 ######################################################################
-#  U T I L I T Y   F U N C T I O N S
-######################################################################
-
-
-def init_db():
-    """ Initializes the SQLAlchemy app """
-    global app
-    Customer.init_db(app)
-
 # UPDATE AN EXISTING CUSTOMER
 ######################################################################
-
-
 @app.route("/customers/<int:customer_id>", methods=["PUT"])
-def update_customer(customer_id):
+def update_customers(customer_id):
     """Update a Customer
 
     This endpoint will update a Customer based the body that is posted
@@ -138,10 +105,15 @@ def update_customer(customer_id):
 
 
 ######################################################################
+# DELETE A CUSTOMER
+######################################################################
+@app.route("/customers", methods=[""])
+def delete_customer(name):
+    return ""
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,18 +1,16 @@
-"""
-TestYourResourceModel API Service Test Suite
+""" 
+Customer API Service Test Suite
 
 Test cases can be run with the following:
-  nosetests -v --with-spec --spec-color
-  coverage report -m
-"""
-import os
-import logging
-from unittest import TestCase
-from unittest.mock import MagicMock, patch
-from service import status  # HTTP Status Codes
-from service.models import db
-from service.routes import app, init_db
+nosetests -v --with-spec --spec-color
+coverage report -m
+codecov -token=$CODECOV_TOKEN
 
+While debugging just these tests it's convenient to use this:
+    nosetests --stop tests/test_routes.py:TestCustomerServer
+"""
+
+from http.client import responses
 import os
 import logging
 import unittest
@@ -25,7 +23,7 @@ from .factories import CustomerFactory
 
 # Disable all but critical errors during normal test run
 # uncomment for debugging failing tests
-logging.disable(logging.CRITICAL)
+#logging.disable(logging.CRITICAL)
 
 # DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
 DATABASE_URI = os.getenv(
@@ -34,13 +32,12 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/customers"
 CONTENT_TYPE_JSON = "application/json"
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-
-
-class TestYourResourceServer(TestCase):
-    """ REST API Server Tests """
+class TestCustomerServer(unittest.TestCase):
+    """ Customer API Server Tests """
 
     @classmethod
     def setUpClass(cls):
@@ -50,8 +47,6 @@ class TestYourResourceServer(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        """ This runs once before the entire test suite """
-        pass
 
     @classmethod
     def tearDownClass(cls):
@@ -85,63 +80,36 @@ class TestYourResourceServer(TestCase):
         return customers
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  T E S T   C A S E S
     ######################################################################
 
     def test_index(self):
         """ Test index call """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], "Customer Service")
 
     # Test create customer account
     def test_create_a_customer(self):
         """Create a new Customer Account"""
-        customer = CustomerFactory()
-        logging.debug(customer)
+        test_customer = CustomerFactory()
+        logging.debug(test_customer)
         resp = self.app.post(
-            BASE_URL, json=customer.serialize(), content_type=CONTENT_TYPE_JSON
+            BASE_URL, json=test_customer.serialize(), content_type=CONTENT_TYPE_JSON
         )
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-
         # Make sure location header is set
         location = resp.headers.get("Location", None)
         self.assertIsNotNone(location)
-
         # Check the data is correct
         new_customer = resp.get_json()
-        self.assertEqual(
-            new_customer["first_name"], customer.first_name,
-            "First name does not match"
-        )
-        self.assertEqual(
-            new_customer["last_name"], customer.last_name, "Last name does not match"
-        )
-        self.assertEqual(
-            new_customer["email"], customer.email, "Email does not match"
-        )
-        self.assertEqual(
-            new_customer["phone_number"], customer.phone_number, "Phone number does not match"
-        )
+        self.assertEqual(new_customer["first_name"], test_customer.first_name,"First name does not match")
+        self.assertEqual(new_customer["last_name"], test_customer.last_name, "Last name does not match")
+        self.assertEqual(new_customer["email"], test_customer.email, "Email does not match")
+        self.assertEqual(new_customer["phone_number"], test_customer.phone_number, "Phone number does not match")
 
-        # Check that the location header was correct
-        resp = self.app.get(location, content_type=CONTENT_TYPE_JSON)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        new_customer = resp.get_json()
-        self.assertEqual(
-            new_customer["first_name"], customer.first_name,
-            "First name does not match"
-        )
-        self.assertEqual(
-            new_customer["last_name"], customer.last_name, "Last name does not match"
-        )
-        self.assertEqual(
-            new_customer["email"], customer.email, "Email does not match"
-        )
-        self.assertEqual(
-            new_customer["phone_number"], customer.phone_number, "Phone number does not match"
-        )
-
-    # Test create customer account with missing data
+    #Test create customer account with missing data
     def test_create_a_customer_no_data(self):
         """Create a Customer with missing data"""
         resp = self.app.post(BASE_URL, json={}, content_type=CONTENT_TYPE_JSON)
@@ -160,15 +128,16 @@ class TestYourResourceServer(TestCase):
         # update the customer
         new_customer = resp.get_json()
         logging.debug(new_customer)
-        new_customer["category"] = "unknown"
+        new_customer["email"] = "new@email.com"
         resp = self.app.put(
-            "/customer/{}".format(new_customer["id"]),
+            "/customers/{}".format(new_customer["id"]),
             json=new_customer,
             content_type=CONTENT_TYPE_JSON,
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_customer = resp.get_json()
-        self.assertEqual(updated_customer["category"], "unknown")
+        logging.debug(updated_customer)
+        self.assertEqual(updated_customer["email"], "new@email.com")
 
     def test_get_customer(self):
         """Get a single customer"""
